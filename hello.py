b8ddb98d06c5f3d17b0a742ac2cdd7492ba64fb3
Cecilia-R28 <ceciliarazakaharisoa@gmail.com>
2026-05-28 23:45:17 +0300

    sending the basis code like hello world and calculator

---
+++ hello.py
@@ -0,0 +1,24 @@
+#Fonctions
+def main():
+    x= int(input("Entrer un nombre x: "))
+    print("le double de x est:", multipli(x))
+
+def multipli(i):
+    return i*i
+main()
+
+def Hello (to):
+    print("hello, " +to)
+name1= input("C'est quoi ton nom?")
+Hello(name1)
+
+print("hello, world")
+name= input("what's your name?")
+print("hello, " +name)
+
+#Calucl  avec +, -, *, /, %
+a= int(input("Entrer un nombre: "))
+b= int(input("Entrerer un autre nombre:"))
+c=a+b
+print("la somme de a + b est "+ str(c))
+
